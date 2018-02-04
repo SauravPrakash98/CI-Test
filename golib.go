e8c80e3336ca0faa55814461dc8e08026028a2aa
Saurav Prakash <sauravprakash98@gmail.com>
2018-02-04 15:48:23 +0530

    build success

--- golib.go
+++ golib.go
@@ -1,31 +1,54 @@
 package main
 
-import (
-	"fmt"
-)
+func cnum(s string) int {
+	var x int
+	x = (int(s[0]-'0'))*100 + (int(s[1]-'0'))*10 + (int(s[2] - '0'))
+	return x
+}
 
 func Validip(str string) int {
-
 	var k int
 	for i := 0; i < len(str); i++ {
-		if str[i] == '.' || i == len(str) {
+		if str[i] == '.' || i == len(str)-1 {
+			var s string
+			var num int
+			if i == len(str)-1 {
+				s = str[k : i+1]
 
-			str1 := str[k:i]
-			fmt.Println(str1)
-			k = i + 1
-			//	fmt.Print(k)
-			if len(str1) >= 4 {
-				fmt.Println("Invalid IP")
 			} else {
-				num := (str1[0]-'0')*100 + (str1[1]-'0')*10 + (str[2] - '0')
-				//fmt.Println(num)
-				if num >= 255 {
-					fmt.Println("Invalid IP")
-				}
+				s = str[k:i]
 			}
+			if len(s) < 4 {
+				num = cnum(s)
+				if num > 256 {
+					return 0
+				}
+			} else {
+				return 0
+			}
+
+			if i != len(str)-1 {
+				k = i + 1
+			}
+		}
+	}
+	return 1
+}
+
+func Nonalpha(str string) string {
+	//var str string
+	//	str = "a..s($4.;'1241d135"
+	//	fmt.Println(str)
+	var s []byte
+	for i := 0; i < len(str); i++ {
+		if (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= '0' && str[i] <= '9') {
+			s = append(s, str[i])
+			//fmt.Println(i)
+		} else {
+			//str = strings.Trim(str, string(str[i]))
 		}
 
 	}
-	fmt.Print("valid IP")
-	return 1
+	st := string(s)
+	return st
 }
--- golib_test.go
+++ golib_test.go
@@ -4,8 +4,16 @@ import "testing"
 
 func TestValidip(t *testing.T) {
 
-	ret := Validip("str")
+	ret := Validip("192.168.043.005")
 	if ret != 1 {
 		t.Errorf("Correct check not done")
+	}
+}
+
+func TestNonalpha(t *testing.T) {
+
+	ret := Nonalpha("sa;[];")
+	if ret != "sa" {
+		t.Errorf("wrong output")
 	}
 }
